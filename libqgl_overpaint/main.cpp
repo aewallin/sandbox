5e5dc9bc4d8f0de6a3060ed8d96591bafbf47169
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-10-04 20:10:49 +0300

    libqglviewer overpaint example

---
+++ libqgl_overpaint/main.cpp
@@ -0,0 +1,11 @@
+#include "viewer.hpp"
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+    QApplication application(argc, argv);
+    Viewer viewer;
+    viewer.setWindowTitle("overpainting");
+    viewer.show();
+    return application.exec();
+}
