617bf57d6e9d928401fdf59acaab3c3c4b6d0c00
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-12-22 15:54:52 +0200

    bgl iterator example/test

---
+++ bgl_iterator/main.cpp
@@ -0,0 +1,108 @@
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <boost/foreach.hpp> 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+
+#define OUT_EDGE boost::listS
+#define VERTEX_LIST boost::vecS
+
+typedef boost::adjacency_list_traits<OUT_EDGE, 
+                                     VERTEX_LIST, 
+                                     boost::bidirectionalS, 
+                                     boost::listS >::edge_descriptor Edge;
+                                     
+struct EdgeProperty {
+    Edge next;
+};
+
+typedef boost::adjacency_list< OUT_EDGE,               // out-edge storage
+                               VERTEX_LIST,            // vertex set storage
+                               boost::bidirectionalS,  // directed tag
+                               boost::no_property,     // vertex properties
+                               EdgeProperty,           // edge properties
+                               boost::no_property,     // graph-properties 
+                               boost::listS            // edge storage
+                               > Graph;
+
+typedef boost::graph_traits< Graph >::edge_descriptor      Edge;
+typedef boost::graph_traits< Graph >::edge_iterator        EdgeItr;
+typedef boost::graph_traits< Graph >::vertex_descriptor    Vertex;
+
+
+class edge_iterator : public boost::iterator_facade<
+               edge_iterator,
+               Edge,
+               boost::forward_traversal_tag> 
+{
+public:
+    edge_iterator(): m_edge( 0 ) {}
+    explicit edge_iterator(Graph* g, Edge* e): m_edge(e), m_g(g)  {}
+private:
+    friend class boost::iterator_core_access;
+    void increment() { m_edge = &( (*m_g)[*m_edge].next ); } 
+    bool equal( edge_iterator const& other) const {
+        return *(this->m_edge) == *(other.m_edge);
+    }
+    Edge& dereference() const { return *m_edge; } 
+    
+    Edge* m_edge;
+    Graph* m_g;
+}; 
+
+int main(int,char*[]) {
+    Graph g;
+
+    Vertex v1 = boost::add_vertex(g);
+    Vertex v2 = boost::add_vertex(g);
+    Vertex v3 = boost::add_vertex(g);
+    Vertex v4 = boost::add_vertex(g);
+    bool tmp;
+    Edge e1,e2,e3,e4;
+    boost::tie(e1, tmp) = boost::add_edge(v1,v2,g);
+    boost::tie(e2, tmp) = boost::add_edge(v2,v3,g);
+    boost::tie(e3, tmp) = boost::add_edge(v3,v4,g);
+    boost::tie(e4, tmp) = boost::add_edge(v4,v1,g);
+    
+    g[e1].next = e2;
+    g[e2].next = e3;
+    g[e3].next = e4;
+    g[e4].next = e1;
+    
+    // 1st way to iterate around face of graph
+    Edge current=e1;
+    Edge start=current;
+    std::cout << "1st way:\n";
+    do {
+        std::cout << current << "\n";
+        current = g[current].next;
+    } while (current!=start);
+    // output:
+    // 1st way:
+    // (0,1)
+    // (1,2)
+    // (2,3)
+    // (3,0)
+
+    
+    // 2nd way to iterate around face of graph
+    edge_iterator begin(&g,&e1);
+    // edge_iterator end(&g,&e1); // this gives no output at all!
+    // edge_iterator end(&g,&e2); // this gives (0,1)
+    // edge_iterator end(&g,&e4); // this gives (0,1) (1,2) (2,3)   (but NOT (3,0) !)
+    Edge e1_cpy = e1;
+    edge_iterator end(&g,&e1_cpy); // this gives no output at all!
+    
+    std::pair<edge_iterator,edge_iterator> face_edges(begin,end);
+    std::cout << "2nd way:\n";
+    BOOST_FOREACH(Edge e, face_edges) {
+        std::cout << e << "\n";
+    }
+    
+    std::cout << " num_verts= " << boost::num_vertices(g) << std::endl;
+    std::cout << " num_edges= " << boost::num_edges(g) << std::endl;
+    return 0;
+}
--- bgl_vertex_desc/main.cpp
+++ bgl_vertex_desc/main.cpp
@@ -5,7 +5,7 @@
 
 #include <boost/graph/adjacency_list.hpp>
 
-#define OUT_EDGE boost::vecS
+#define OUT_EDGE boost::listS
 #define VERTEX_LIST boost::listS
 
 typedef boost::adjacency_list_traits<OUT_EDGE, 
@@ -16,7 +16,7 @@ typedef boost::adjacency_list_traits<OUT_EDGE,
 // edge properties
 struct EdgeProperty {
     EdgeProperty() {
-        space_eater.resize(100);
+        space_eater.resize(1000);
     }
     Edge twin;
     std::vector<double> space_eater;
@@ -62,8 +62,18 @@ void check_all_edges(Graph& g) {
     }
 }
 
+void print_edges(Graph& g) {
+    EdgeItr it, end;
+    boost::tie( it, end) = boost::edges(g);
+    std::cout << " edges :\n";
+    for( ; it!=end ; it++) {
+        std::cout << *it << "\n";
+    }
+}
+
 int main(int,char*[]) {
     Graph g;
+    /*
     int Nmax=1000;
     for (int m=0;m<Nmax;m++) {
         Vertex v1 = boost::add_vertex(g);
@@ -76,7 +86,44 @@ int main(int,char*[]) {
         g[e2].twin = e1;
         check_edge(e1,g);
         check_edge(e2,g);
-    }
+    }*/
+    Vertex v1 = boost::add_vertex(g);
+    Vertex v2 = boost::add_vertex(g);
+    Vertex v3 = boost::add_vertex(g);
+    Vertex v4 = boost::add_vertex(g);
+    bool tmp;
+    Edge e1,e2,e3,e4;
+    boost::tie(e1, tmp) = boost::add_edge(v1,v2,g);
+    boost::tie(e2, tmp) = boost::add_edge(v2,v1,g);
+    g[e1].twin = e2;
+    g[e2].twin = e1;
+    
+    boost::tie(e3, tmp) = boost::add_edge(v2,v3,g);
+    boost::tie(e4, tmp) = boost::add_edge(v3,v2,g);
+    g[e3].twin = e4;
+    g[e4].twin = e3;
+    
+    check_all_edges(g);
+    print_edges(g);
+    boost::remove_edge(e3,g);
+    boost::remove_edge(e4,g);
+    boost::remove_vertex( v3 , g );
+    std::cout << " vertex v3 = " << v3 << "\n";
+    //print_edges(g);
+    //check_all_edges(g);sudo
+    Vertex v5 = boost::add_vertex(g);
+    Vertex v6 = boost::add_vertex(g);
+    std::cout << " vertex v5 = " << v5 << "\n";
+    Edge e5, e6;
+    boost::tie(e5, tmp) = boost::add_edge(v5,v6,g);
+    boost::tie(e6, tmp) = boost::add_edge(v6,v5,g);
+    g[e5].twin = e6;
+    g[e6].twin = e5;
+    
+    boost::add_vertex(g);
+    boost::add_vertex(g);
+    boost::remove_vertex( v3 , g );
+    print_edges(g);
     check_all_edges(g);
     
     std::cout << " num_verts= " << boost::num_vertices(g) << std::endl;
