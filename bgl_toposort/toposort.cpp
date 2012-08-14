43d7d2b7ce618f0022ca00de7a99854431fb5766
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-08-14 21:16:51 +0300

    tidy

--- bgl_toposort/toposort.cpp
+++ bgl_toposort/toposort.cpp
@@ -7,30 +7,22 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/topological_sort.hpp>
 
-struct VertexProperty {
-    int index;
-};
-
 int main(int,char*[]) {
     using namespace std;
     using namespace boost;
 
-    typedef adjacency_list<    vecS,               // out-edge storage
+    typedef adjacency_list<    vecS,            // out-edge storage
                                vecS,            // vertex set storage
-                               directedS,  // directed tag
-                               no_property, //VertexProperty,     // vertex properties
-                               no_property,           // edge properties
+                               directedS,       // directed tag
+                               no_property,     // vertex properties
+                               no_property,     // edge properties
                                no_property,     // graph-properties 
                                listS            // edge storage
                                > Graph;
                                
     typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
-    //typedef pair<int, int> Pair;
 
-
-    //Pair edges[6] = { Pair(0,1), Pair(2,4), Pair(2,5),
-    //                  Pair(0,3), Pair(1,4), Pair(4,3) };
-    Graph G(6); //, edges, edges + 6);
+    Graph G(6);
     add_edge( 0,1,G);
     add_edge( 2,4,G);
     add_edge( 2,5,G);
@@ -46,6 +38,8 @@ int main(int,char*[]) {
     for ( container::reverse_iterator ii=c.rbegin(); ii!=c.rend(); ++ii)
     cout << (*ii) << " ";
     cout << endl;
-    
+    // output:
+    // A topological ordering: 2 5 0 1 4 3 
+
     return 0;
 }
