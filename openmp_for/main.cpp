5b1cd45bd85cc9060bb39b7afc9302a0a6a68c7f
Anders Wallin <anders.e.e.wallin@gmail.com>
2019-06-03 22:19:49 +0300

    reduction

--- openmp_for/main.cpp
+++ openmp_for/main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
-#include <cstdio> 
+#include <cstdio>
 #include <ctime>
+#include <iostream>
 
 void omp_parallel_test(void) {
 #pragma omp parallel
@@ -35,8 +36,39 @@ void omp_forloop() {
 
 }
 
+void omp_reduction_array(void)
+{
+// from http://dvalts.io/optimisation/code/2016/11/06/OpenMP-array_reduction.html
+  int myArray[6] = {};
+  // Print the array elements to see them before
+  std::cout << "Array before run: " << std::endl;
+  for (int n = 0; n<6; ++n)
+  {
+    std::cout << myArray[n] << " " << std::endl;
+  } 
+
+
+  #pragma omp parallel for reduction(+:myArray[:6])
+  for (int i=0; i<50; ++i)
+  {
+    double a = 2.0; // Or something non-trivial justifying the parallelism...
+    for (int n = 0; n<6; ++n)
+    {
+      myArray[n] += a;
+    }
+  }
+  std::cout << "Array after run: " << std::endl;
+  // Print the array elements to see them summed
+  // add 2 50-times to each element = 100.
+  for (int n = 0; n<6; ++n)
+  {
+    std::cout << myArray[n] << " " << std::endl;
+  } 
+}
+
 int main()
 {
 	omp_parallel_test();
 	omp_forloop();
+	omp_reduction_array();
 }
